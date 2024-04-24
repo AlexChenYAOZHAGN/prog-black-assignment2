2c0227df5e4666cbe763b3fa3479ee6f444e7e3b
ZnDream <134977697+ZnDream@users.noreply.github.com>
2024-04-24 02:27:59 +0100

    Update search.js

--- js/pages/search.js
+++ js/pages/search.js
@@ -2,13 +2,19 @@ import { searchProduct } from "../../mockdata/products.js";
 import { renderProductGrid } from "../components/productGrid.js";
 import { renderHeader } from "../components/header.js";
 
-renderHeader();
+document.addEventListener('DOMContentLoaded', () => {
+    renderHeader();
 
-const params = new URLSearchParams(location.search);
-const products = searchProduct(
-    params.get("input"),
-    parseFloat(params.get("minPrice") || 0),
-    parseFloat(params.get("maxPrice") || Number.MAX_SAFE_INTEGER)
-);
-console.log(products)
-renderProductGrid(products);
+    if (window.location.search) {
+        const params = new URLSearchParams(window.location.search);
+        const products = searchProduct(
+            params.get("input"),
+            parseFloat(params.get("minPrice") || 0),
+            parseFloat(params.get("maxPrice") || 1000000) 
+        );
+        console.log(products);
+        renderProductGrid(products);
+    } else {
+        console.log("No search parameters found.");
+    }
+});
